94eb146e7545c44000c2fc15519fd91796e70777
Michael Hughes <microneesia79@gmail.com>
2026-09-05 13:48:52 -0700

    Mount place search and desktop scroll hardening

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import './styles.css'
 import './v04.css'
 import './robustness.css'
 import './ledger.css'
+import './v07.css'
 import App from './App.jsx'
+import AddressSearchPortal from './AddressSearchPortal.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
+      <AddressSearchPortal />
     </ErrorBoundary>
   </React.StrictMode>,
 )
