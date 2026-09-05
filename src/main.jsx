80a3b7ab110b507b06b01433dbcbc2d479c7177c
Michael Hughes <microneesia79@gmail.com>
2026-09-05 12:47:17 -0700

    Load v0.4 interface styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import './styles.css'
+import './v04.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
