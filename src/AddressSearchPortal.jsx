c4dac1b3cf80b0993304188b8a6290139de238c3
Michael Hughes <microneesia79@gmail.com>
2026-09-05 14:43:46 -0700

    Stamp Peekaboo v0.8 release label

--- src/AddressSearchPortal.jsx
+++ src/AddressSearchPortal.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 import AddressSearch from './AddressSearch.jsx'
 
-const RELEASE_LABEL = 'PEEKABOO v0.7'
+const RELEASE_LABEL = 'PEEKABOO v0.8'
 
 function zoomForPlace(place) {
   if (!place?.bounds) return 16
