3dd7ddf9888c96e74b6ae7d75413f7f6653fe3ce
Michael Hughes <microneesia79@gmail.com>
2026-09-05 20:05:30 -0700

    Bump Peekaboo to v2.1.0

--- src/release.js
+++ src/release.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = '2.0.0'
+export const APP_VERSION = '2.1.0'
 export const RELEASE_LABEL = `PEEKABOO v${APP_VERSION}`
-export const RELEASE_SHORT = 'v2.0'
+export const RELEASE_SHORT = 'v2.1'
